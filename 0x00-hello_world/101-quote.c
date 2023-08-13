456fbb6a4d214808906501075561a1d1af0bbad3
Islam Farghaly <islam.farghaly@outlook.com>
2023-08-13 12:13:53 -0700

    write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,16 @@
 /**
  * main - Entry point
  *
- * description: print using write function    write(int fd, void* buf, ssize cnt) 
- *  The first parameter (fd) is the file descriptor where you want to write the file description. The data that has to be written in the second parameter. At last, the third parameter is the total bytes that has to be written
- *   
- * Return: 1
+ * description: print using write function
+ * write(int fd, void* buf, ssize cnt)
+ *
+ * Return: 1 (error)
 */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,quo,59);	
+
+	write(1, quo, 59);
 	return (1);
 }
